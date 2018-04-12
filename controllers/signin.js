f6b24eb3f1634cc90ddd4b11cd2d5ec0db80aeda
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-04-12 17:36:19 +0800

    第一步改造

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,13 +7,13 @@ module.exports = {
             password = ctx.request.body.password || '';
 
         if (email === 'admin@example.com' && password === '123456') {
-            console.log('signin ok!');
+            // console.log('signin ok!');
             ctx.render('signin-ok.html', {
                 title: 'Sign In OK',
                 name: 'Mr Node'
             });
         } else {
-            console.log('signin failed!');
+            // console.log('signin failed!');
             ctx.render('signin-failed.html', {
                 title: 'Sign In Failed'
             });
