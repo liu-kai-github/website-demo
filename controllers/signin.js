fe69c5a964d5587d7978c6c13be12addc1ecdbb4
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-04-16 14:18:39 +0800

    联调数据库成功

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,13 +1,15 @@
 // sign in:
+const executeSQL = require('../models/sql');
 
 module.exports = {
     'POST /signin': async (ctx, next) => {
-        // SELECT * FROM `users_login_info` WHERE email = 'liukai@gmail.com' AND password = '123456';s
+        // SELECT * FROM `users_login_info` WHERE email = 'liukai@gmail.com' AND password = '123456';
         const
             email = ctx.request.body.email || '',
             password = ctx.request.body.password || '';
+        const users = await executeSQL('SELECT * FROM `users_login_info` WHERE email = ? AND password = ?', [email, password]);
 
-        if (email === 'admin@example.com' && password === '123456') {
+        if (users.length > 0) {
             // console.log('signin ok!');
             ctx.render('signin-ok.html', {
                 title: 'Sign In OK',
